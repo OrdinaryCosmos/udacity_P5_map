bc6eab41cbfdd8de999ab6365292a21922a56187
OrdinaryCosmos <liucosmos1@gmail.com>
2015-03-28 00:29:13 -0400

    add the instructions and comments

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # udacity_P5_map
 
-it is my first try on project 5,definitely the most difficult one so far!
+1. Open the index.html page in your browser, you can see a google map of Manhattan and the marks for four major
+landmarks: New York University, IFC Center, New York City Hall and New York Stock Exchange. These marks are
+also listed at the upper-right corner of the map.
+
+2. You can click on each of the mark to trigger a pop-up info window. Displayed inside the info window
+are link to this place's Foursquare page, its telephone number, its picture(also taken from Foursquare) and how many
+likes it got on foursquare.
+
+3. You can also activate this info window by click on the "Details" button in the list at the pper-right corner of the map.
+After being clicked, the corresponding info window will pop up.
+
+4.At the lower-left corner is a search bar which has the auto-completion function.After the user begins to type, the names of the landmarks which contain the same letters users have type will appear above the search bar.User can select the landmarks they want to know more about and click search, which will make the corresponding landmark pop up in the map.
--- map.js
+++ map.js
@@ -3,10 +3,14 @@
  */
 function initialize() {
     var mapOptions = {
-        center: { lat: 40.722505, lng: -73.992985},
+        center: { lat: 40.722505, lng: -73.992985},//initial the map centering at new york city
         zoom: 14
     };
     var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+    /* create the markers for each landmark
+    */
+
     var NYSEmarker = new google.maps.Marker({
         position:new google.maps.LatLng(40.706816, -74.011260),
         map: map,
@@ -31,14 +35,18 @@ function initialize() {
         title:"NY city hall"
     });
 
-    var contentHtml='<div><div><a href="%url%" >%name%</a> <span>%likes%</span></div><div><img src="%imgurl%" alt=""><span>Tel: %phone% </span></div></div>'
+
+    //the template for info window
+    var contentHtml='<div><div><a href="%url%" >%name%</a> <span>%likes%</span></div><div><img src="%imgurl%" alt=""><span>Tel: %phone% </span></div></div>';
 
 
     var markers={'New York Stock Exchange':{"marker":NYSEmarker,"id":'431f7f00f964a52085271fe3'},'New York University':{"marker":NYUmarker,"id":'5058de7be4b007870dd7ad7a'},'New York City Hall':{"marker":CITYHallmarker,"id":'4a676321f964a52051c91fe3'},'IFC Center':{"marker":IFCmarker,"id":"42f00900f964a520b9261fe3"}};
 
+    //api for foursquare
     var apiurl="https://api.foursquare.com/v2/venues/%data%?client_id=4VL2YZKWW1PDC2OU5DMRDSLQH0OTJYNFAKPO4JFXYOKMEDJP&client_secret=JB5ZVQON05C5WR2U5FEWVYZLX1XL2JMVLEHVBPEAVO2CMBMM&v=20150315";
-    for (var key in markers) {
-        (function(realkey){
+
+    //to get infomation from api construct the info window for a marker
+    var addMarkers=function(realkey){
         var clickMarker=markers[realkey];
         var venueurl=apiurl.replace('%data%',clickMarker.id);
         var contentString;
@@ -48,8 +56,7 @@ function initialize() {
             dataType: "jsonp",
             success: function( response ) {
                 var clickVenue=response.response.venue;
-                console.log( clickVenue );
-                 contentString=contentHtml.replace('%url%',clickVenue.canonicalUrl).replace('%name%',clickVenue.name).replace('%likes%',clickVenue.likes.summary).replace('%imgurl%',clickVenue.bestPhoto.prefix+'150x100'+clickVenue.bestPhoto.suffix ).replace('%phone%',clickVenue.contact.formattedPhone);
+                contentString=contentHtml.replace('%url%',clickVenue.canonicalUrl).replace('%name%',clickVenue.name).replace('%likes%',clickVenue.likes.summary).replace('%imgurl%',clickVenue.bestPhoto.prefix+'150x100'+clickVenue.bestPhoto.suffix ).replace('%phone%',clickVenue.contact.formattedPhone);
                 var infowindow = new google.maps.InfoWindow({
                     content: contentString
                 });
@@ -57,22 +64,29 @@ function initialize() {
                     infowindow.open(map,clickMarker.marker);
                 });
             }
-        });})(key);
+        });};
+
+    //loop through the markers array to add info window for each of the marker
+    for (var key in markers) {
+        if (markers.hasOwnProperty(key)) {
+            addMarkers(key);
+        }
     }
 
 
-
+    //knockout.js setup
     function AppViewModel()  {
         var self=this;
-        self.items =  [
+
+        self.items =  [//for autocomplete
             'New York Stock Exchange',
             'New York University',
             'New York City Hall',
             'IFC Center'
         ];
-        self.places = ko.observableArray(['New York Stock Exchange', 'New York University','New York City Hall','IFC Center' ]);
-        self.condition= ko.observable(),
-        self.beginSearch=function(keyword) {
+        self.places = ko.observableArray(['New York Stock Exchange', 'New York University','New York City Hall','IFC Center' ]);//use knockout js to loop throught this array to create list view
+        self.condition= ko.observable();//the model for key word in search bar
+        self.beginSearch=function(keyword) {//what happen when search button is clicked
                 for (var key in markers) {
                     if (key.toLowerCase()===keyword.toLowerCase()) {
                         markers[key].marker.setMap(map);
@@ -85,7 +99,9 @@ function initialize() {
 
                 }
             };
-    };
+    }
+
+    //for autocomplete to work.
     AppViewModel.prototype.onSelect = function(ev, ui) {
         this.condition(ui.item.value);
     };
@@ -94,4 +110,6 @@ function initialize() {
 
 
 }
+
+//load page;
 google.maps.event.addDomListener(window, 'load', initialize);
